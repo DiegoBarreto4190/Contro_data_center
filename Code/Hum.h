338602ba9370f562d0cd4024dcc04565a6a7aa5d
DiegoBarreto4190 <59875148+DiegoBarreto4190@users.noreply.github.com>
2021-02-27 01:14:03 -0300

    Update Hum.h

--- Code/Hum.h
+++ Code/Hum.h
@@ -1,17 +1,16 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <AM2320.h
-
 #define Sensor_1 0 //Colocar el pin que corresponda
-AM2320 Sensor_2;
 
+AM2320 Sensor_2;
 DHT HS1(Sensor_1, DHT21);
 
-float _H1(){
+float _h1(){
   return HS1.readHumidity();
 }
 
-float _H1(){
+float _h2(){
   if (Sensor_2.easure()) {
     return(Sensor_2.getHumidity());
   }
